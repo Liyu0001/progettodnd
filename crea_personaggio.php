8f40de7bac8742ac53623e871dce946173576ced
Liyu0001 <sonarhate@gmail.com>
2024-04-10 15:58:39 +0200

    correzioni generali

--- crea_personaggio.php
+++ crea_personaggio.php
@@ -6,14 +6,18 @@
    require('Dado.php');
    $d20 = new  Dado(20); $d12 = new Dado(12);$d8 = new Dado(8);$d6 = new Dado(6);$d4 = new Dado(4);   
    $indiceRazza = $_POST["razza"];
-   $nomePersonaggio = $_POST["nome"];  
-/*     $stats = $_POST["stats"]; */
-/* $personaggio = new Personaggio($nomePersonaggio,100,10,$razze[$indiceRazza], $stats); */
-/* 
-echo "è stato creato $nomePersonaggio";
-var_dump($personaggio);  */
+   $nomePersonaggio = $_POST["nome"];
+   $stats = $_POST["stats"];
+   // funzione per la converzione dei valori stringa $_POST in interi.
+   function conversioneValoriInteri($value) {
+      return intval($value);
+   };
+  $stats = array_map("conversioneValoriInteri",$stats);   
+   
 
-var_dump($_POST["stats"]);
+ $personaggio = new Personaggio($nomePersonaggio,100,10,$razze[$indiceRazza], $stats); 
+
+echo "è stato creato $nomePersonaggio";
 /* 
 $game= new scontro();
 $p1 = new Personaggio($nomePersonaggio,100,10,$razze[$indiceRazza],$_POST["stats"]);
